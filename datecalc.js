5edb8a8b9a81b91d082cca90a38bb82fc65c2614
Stuart Leitch <stuart.leitch@leitches.net>
2023-07-06 01:28:55 +0100

    Introduce datebox class to abstract input field

--- datecalc.js
+++ datecalc.js
@@ -1,50 +1,50 @@
+
+class DateBox {
+  constructor(id, chg) {
+    this.id = id;
+    this.whenChanged = chg;
+    this.set(new Date, false);
+    addListener(this.id, "change", this.updated.bind(this));
+  }
+  set(value, update = true) {
+    this.value = value;
+    document.getElementById(this.id).value = this.value.toISOString().substring(0, 10);
+    this.value = new Date(document.getElementById(this.id).value);
+    if (update) { eval(this.whenChanged + "()") }
+  }
+  updated() {
+    this.value = new Date(document.getElementById(this.id).value);
+    eval(this.whenChanged + "()")
+  }
+}
+
+
 window.onload = initialise;
 
 function initialise() {
   // event listeners
-  addChangeListener("diff-start-date", calc_difference)
-  addChangeListener("diff-end-date", calc_difference)
-  addChangeListener("end-date-start", calc_end_date)
-  addChangeListener("end-date-weeks", calc_end_date)
+  addListener("end-date-weeks", "change", calc_end_date)
+  addListener("set-end-date-as-start-btn", "click", set_as_start)
+  addListener("add-block-to-list-btn", "click", add_block)
 
-  addClickListener("set-end-date-as-start-btn", set_as_start)
-  addClickListener("add-block-to-list-btn", add_block)
+  diffStartDate = new DateBox("diff-start-date", "calc_difference");
+  diffEndDate = new DateBox("diff-end-date", "calc_difference");
+  calc_difference();
 
-  // Set default values, after adding event listeners :)
-  set_to_today('diff-start-date');
-  set_to_today("diff-end-date");
-  set_to_today('end-date-start');
-  set_to_today('block-start-date');
+  calcStart = new DateBox("end-date-start", "calc_end_date");
+  calcEnd = new Date()
+  calc_end_date();
 
+  blockStart = new DateBox("block-start-date", "calc_end_date");
 }
 
 function addListener(elem, type, fn) {
   document.getElementById(elem).addEventListener(type, fn);
 }
-function addChangeListener(elem, fn) {
-  addListener(elem, "change", fn);
-}
-function addClickListener(elem, fn) {
-  addListener(elem, "click", fn);
-}
-
-function hasChanged(element_id) {
-  const ev = new Event('change');
-  document.getElementById(element_id).dispatchEvent(ev);
-}
-
-function set_to_today(element_id) {
-  var today = new Date();
-  document.getElementById(element_id).value = today.toISOString().substring(0, 10);
-
-  // const ev = new Event('change');
-  // document.getElementById(element_id).dispatchEvent(ev);
-  hasChanged(element_id);
-}
 
 function calc_difference() {
-  var start = new Date(document.getElementById("diff-start-date").value);
-  var end = new Date(document.getElementById("diff-end-date").value);
+  var start = diffStartDate.value;
+  var end = diffEndDate.value;
   var diff = end - start; //difference in milliseconds
 
   var days = diff / (1000 * 60 * 60 * 24);
@@ -53,22 +53,22 @@ function calc_difference() {
   var weeks = Math.floor(days / 7) //round to nearest whole number
   var weeks_days = days % 7;
   document.getElementById("weeks").innerHTML = weeks + ' weeks' + ' and ' + weeks_days + ' days';
-
 }
 
 function calc_end_date() {
-  var start = new Date(document.getElementById("end-date-start").value);
-  var weeks = document.getElementById("end-date-weeks").value;
-  var end = new Date(start.getTime() + (weeks * 7 * 24 * 60 * 60 * 1000));
+  // var start = new Date(document.getElementById("end-date-start").value);
+  var start = calcStart.value;
 
-  document.getElementById("end-date-end").value = end.toISOString().substring(0, 10);
+  var weeks = document.getElementById("end-date-weeks").value;
+  calcEnd = new Date(start.getTime() + (weeks * 7 * 24 * 60 * 60 * 1000));
+
+  // document.getElementById("end-date-end").value = end.toISOString().substring(0, 10);
   document.getElementById("end-date-start-out").innerHTML = start.toDateString();
-  document.getElementById("end-date-end-out").innerHTML = end.toDateString();
+  document.getElementById("end-date-end-out").innerHTML = calcEnd.toDateString();
 }
 
 function set_as_start() {
-  document.getElementById("end-date-start").value = document.getElementById("end-date-end").value;
-  hasChanged("end-date-start");
+  calcStart.set(calcEnd);
 }
 
 function add_block() {
